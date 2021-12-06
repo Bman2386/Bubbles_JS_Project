a6da4e3788b656671a574765676594b8346f93a2
Brendon <brendon.biagi@gmail.com>
2021-12-06 18:12:35 -0500

    add comment for plan of action

--- src/game.js
+++ src/game.js
@@ -217,6 +217,7 @@ export default class Game {
         }
     }
 
+    // dry this code with a collision function that gets called with each vs seperate collision algos
     detectShieldCollision(shield){
         const shieldTop = ((shield.shieldY))
         const shieldBottom = ((shield.shieldY + shield.shieldHeight));
