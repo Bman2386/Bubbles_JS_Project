7685b84c9ecf51200d47fbe49b41b81801e45102
Brendon <brendon.biagi@gmail.com>
2023-04-24 16:57:45 -0400

    remove semicolins

--- src/gameFiles/boss.js
+++ src/gameFiles/boss.js
@@ -2,19 +2,19 @@
 
 export default class Boss {
     constructor(canvasWidth, canvasHeight) {
-        const canvas = document.getElementById('myCanvas');
-        this.ctx = canvas.getContext("2d");
+        const canvas = document.getElementById('myCanvas')
+        this.ctx = canvas.getContext("2d")
         // canvas size
-        this.canvasWidth = canvasWidth;
-        this.canvasHeight = canvasHeight;
+        this.canvasWidth = canvasWidth
+        this.canvasHeight = canvasHeight
         // Boss size
-        this.bossWidth = 200;
-        this.bossHeight = 200;
+        this.bossWidth = 200
+        this.bossHeight = 200
 
-        this.bossHealth = 300;
+        this.bossHealth = 300
         // Boss starting position
-        this.bossX = 200;
-        this.bossY = 75;
+        this.bossX = 200
+        this.bossY = 75
         
         this.imgSrc = "./images/spaceship.jpg"
         // Binding "this" to the drawBoss function so that we don't lose on instantiation
@@ -24,31 +24,31 @@ export default class Boss {
     }
 
     drawBoss(ctx) {
-        const img = new Image();
+        const img = new Image()
         img.src = this.imgSrc
         ctx.drawImage(img, this.bossX, this.bossY, this.bossWidth, this.bossHeight)
-        this.healthBar();
+        this.healthBar()
     }
 
     healthBar() {
-        this.ctx.beginPath(); 
-        this.ctx.rect(this.bossX, (this.bossY-20), 300, 20); 
-        this.ctx.fillStyle = "#FF0000";
-        this.ctx.fill();
+        this.ctx.beginPath()
+        this.ctx.rect(this.bossX, (this.bossY-20), 300, 20)
+        this.ctx.fillStyle = "#FF0000"
+        this.ctx.fill()
 
         this.ctx.beginPath(); 
-        this.ctx.rect(this.bossX, (this.bossY-20), this.bossHealth, 20); 
-        this.ctx.fillStyle = "#339933"; 
-        this.ctx.fill();
+        this.ctx.rect(this.bossX, (this.bossY-20), this.bossHealth, 20)
+        this.ctx.fillStyle = "#339933"
+        this.ctx.fill()
     }
 
 
     bossMove() {
         if (this.direction === "right") {
-            this.bossX += 10;
+            this.bossX += 10
         }
          if (this.direction === "left") {
-            this.bossX -= 10;
+            this.bossX -= 10
         }
         this.boundry()
        
