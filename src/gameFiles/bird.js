092c82248fd1085dd2cb7613ae94e91f28b6c5d7
Brendon <brendon.biagi@gmail.com>
2023-04-24 16:43:56 -0400

    remove semicolins

--- src/gameFiles/bird.js
+++ src/gameFiles/bird.js
@@ -2,15 +2,15 @@ import birdImg from "./images/bird.jpg"
 
 export default class Enemy {
     constructor(canvasWidth, canvasHeight) {
-        const canvas = document.getElementById('myCanvas');
-        this.ctx = canvas.getContext("2d");
-        this.canvasWidth = canvasWidth;
-        this.canvasHeight = canvasHeight;
+        const canvas = document.getElementById('myCanvas')
+        this.ctx = canvas.getContext("2d")
+        this.canvasWidth = canvasWidth
+        this.canvasHeight = canvasHeight
 
-        this.birdHeight = 75;
-        this.birdWidth = 75;
+        this.birdHeight = 75
+        this.birdWidth = 75
 
-        this.birdY = Math.floor(Math.random() * Math.floor(200));
+        this.birdY = Math.floor(Math.random() * Math.floor(200))
         this.birdX = 0
 
         this.drawBird = this.drawBird.bind(this)
@@ -18,9 +18,9 @@ export default class Enemy {
     }
 
     drawBird(ctx) {
-        const img = new Image();
-        img.src = birdImg;
-        ctx.drawImage(img, this.birdX, this.birdY, this.birdWidth, this.birdHeight);
+        const img = new Image()
+        img.src = birdImg
+        ctx.drawImage(img, this.birdX, this.birdY, this.birdWidth, this.birdHeight)
         this.birdX += 7
     }
 }
