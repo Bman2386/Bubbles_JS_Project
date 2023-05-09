e2cf242183bda55c1d29be4a43602f859334ce0f
Brendon <brendon.biagi@gmail.com>
2023-05-09 15:07:30 -0400

    remove semicolins from bubble.js

--- src/gameFiles/bubble.js
+++ src/gameFiles/bubble.js
@@ -1,42 +1,42 @@
-import Sound from "./sound";
+import Sound from "./sound"
 
 export default class Bubble {
     constructor(canvasWidth, canvasHeight) {
-        const canvas = document.getElementById('myCanvas');
-        this.ctx = canvas.getContext("2d");
+        const canvas = document.getElementById('myCanvas')
+        this.ctx = canvas.getContext("2d")
 
-        this.canvasWidth = canvasWidth;
-        this.canvasHeight = canvasHeight;
+        this.canvasWidth = canvasWidth
+        this.canvasHeight = canvasHeight
 
-        this.bubbleWidth = 20;
-        this.bubbleHeight = 20;
+        this.bubbleWidth = 20
+        this.bubbleHeight = 20
 
-        this.bubbleHealth = 20;
+        this.bubbleHealth = 20
 
-        this.bubbleX = 450;
-        this.bubbleY = 200;
-        this.dirX = 0;
-        this.dirY = 0;
+        this.bubbleX = 450
+        this.bubbleY = 200
+        this.dirX = 0
+        this.dirY = 0
 
-        this.score = 0;
-        this.sound = new Sound("src/game/sounds/pop.mp3");
-        this.boostSound = new Sound("src/game/sounds/boost.mp3");
-        this.mute = false;
-        this.imgSrc = "./images/bubble.png";
+        this.score = 0
+        this.sound = new Sound("src/game/sounds/pop.mp3")
+        this.boostSound = new Sound("src/game/sounds/boost.mp3")
+        this.mute = false
+        this.imgSrc = "./images/bubble.png"
 
-        this.drawBubble = this.drawBubble.bind(this);
+        this.drawBubble = this.drawBubble.bind(this)
         this.moveBubble = this.moveBubble.bind(this)
 
-        this.keyDownHandler = this.keyDownHandler.bind(this);
-        this.keyUpHandler = this.keyUpHandler.bind(this);
-        this.stopBubble = this.stopBubble.bind(this);
+        this.keyDownHandler = this.keyDownHandler.bind(this)
+        this.keyUpHandler = this.keyUpHandler.bind(this)
+        this.stopBubble = this.stopBubble.bind(this)
 
-        document.addEventListener("keydown", this.keyDownHandler, false);
-        document.addEventListener("keyup", this.keyUpHandler, false);
+        document.addEventListener("keydown", this.keyDownHandler, false)
+        document.addEventListener("keyup", this.keyUpHandler, false)
     }
 
     drawBubble(ctx){
-        const img = new Image();
+        const img = new Image()
         img.src = this.imgSrc   
         this.bubbleSize(this.bubbleHealth)   
         ctx.drawImage(img, this.bubbleX, this.bubbleY, this.bubbleWidth, this.bubbleHeight)
@@ -44,27 +44,27 @@ export default class Bubble {
 
     bubbleSize(bubbleHealth) {
         if (bubbleHealth === 2) {
-            this.bubbleHeight = 50;
-            this.bubbleWidth = 50;
-            this.imgSrc = "./images/bubble.png";
+            this.bubbleHeight = 50
+            this.bubbleWidth = 50
+            this.imgSrc = "./images/bubble.png"
         } else if (bubbleHealth < 2) {
-            this.bubbleHeight = 20;
-            this.bubbleWidth = 20;
-            this.imgSrc = "./images/bubble.png";
+            this.bubbleHeight = 20
+            this.bubbleWidth = 20
+            this.imgSrc = "./images/bubble.png"
         } else if (bubbleHealth > 30) {
-            this.bubbleHeight = 50;
-            this.bubbleWidth = 50;
+            this.bubbleHeight = 50
+            this.bubbleWidth = 50
             this.imgSrc = "./images/shield.jpg"
         }
 
     }
 
     newPos() {
-        this.bubbleX += this.dirX;
-        this.bubbleY += this.dirY;
+        this.bubbleX += this.dirX
+        this.bubbleY += this.dirY
         
-        this.detectBorders();
-    };
+        this.detectBorders()
+    }
 
     detectBorders() {
         if (this.bubbleX < 3){
@@ -84,55 +84,55 @@ export default class Bubble {
     moveBubble(direction) {
         switch (direction) {
             case "left":
-                this.dirX = -2;
-                break;
+                this.dirX = -2
+                break
             case "right":
-                this.dirX = 2;
-                break;
+                this.dirX = 2
+                break
             case "up":
-                this.dirY = -2;
+                this.dirY = -2
                 break
             case "down":
-                this.dirY = 2;
-                break;
+                this.dirY = 2
+                break
             case "boost":
                 this.mute ? null : this.boostSound.play()
-                this.dirX *= 5;
-                this.dirY *= 5;
+                this.dirX *= 5
+                this.dirY *= 5
                 break
         }
-    };
+    }
 
     stopBubble(direction){
         if (direction === 'horizontal'){
-            this.dirX = 0;
+            this.dirX = 0
         }
         if (direction === 'vertical'){
-            this.dirY = 0;
+            this.dirY = 0
         }
     }
     keyDownHandler(e) {
-        e.preventDefault();
+        e.preventDefault()
         if (e.key == "a" || e.key == "KeyA") {
-            this.moveBubble("left");
+            this.moveBubble("left")
         } else if (e.key == "d" || e.key == "KeyD") {
-            this.moveBubble("right");
+            this.moveBubble("right")
         } else if (e.key == "w" || e.key == "KeyW") {
-            this.moveBubble("up");
+            this.moveBubble("up")
         } else if (e.key == "s" || e.key == "KeyS"){
             this.moveBubble("down")
         } else if ((e.key == " " || e.key =="SpaceBar") && this.bubbleHealth > 30){
             this.moveBubble("boost")
         } 
 
-    };
+    }
 
     keyUpHandler(e) {
-        e.preventDefault();
+        e.preventDefault()
         if (e.key == "a" || e.key == "KeyA" || e.key == "d" || e.key == "KeyD") {
-            this.stopBubble("horizontal");
+            this.stopBubble("horizontal")
         } else if (e.key == "w" || e.key == "KeyW" || e.key == "s" || e.key == "KeyS") {
-            this.stopBubble("vertical");
+            this.stopBubble("vertical")
         } 
-    };
+    }
 }
