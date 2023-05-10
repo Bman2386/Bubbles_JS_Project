ca864c461a86ded94dd2e94ee0c47a061ee5a869
Brendon <brendon.biagi@gmail.com>
2023-05-10 15:17:05 -0400

    fix linter error in clip.js

--- src/gameFiles/clip.js
+++ src/gameFiles/clip.js
@@ -1,24 +1,24 @@
 
 export default class Clip {
     constructor(canvasWidth, canvasHeight) {
-        const canvas = document.getElementById('myCanvas');
-        this.ctx = canvas.getContext("2d");
-        this.canvasWidth = canvasWidth;
-        this.canvasHeight = canvasHeight;
+        const canvas = document.getElementById('myCanvas')
+        this.ctx = canvas.getContext("2d")
+        this.canvasWidth = canvasWidth
+        this.canvasHeight = canvasHeight
 
-        this.clipHeight = 25;
-        this.clipWidth = 25;
+        this.clipHeight = 25
+        this.clipWidth = 25
 
-        this.clipX = Math.floor(Math.random() * Math.floor(900) );
-        this.clipY = Math.floor(Math.random() * Math.floor(500) );
+        this.clipX = Math.floor(Math.random() * Math.floor(900) )
+        this.clipY = Math.floor(Math.random() * Math.floor(500) )
 
-        this.drawClip = this.drawClip.bind(this);
+        this.drawClip = this.drawClip.bind(this)
 
-        this.imgSrc = "./images/paperclip.png";
+        this.imgSrc = "./images/paperclip.png"
     }
 
     drawClip(ctx) {
-        const img = new Image();
+        const img = new Image()
         img.src = this.imgSrc
         ctx.drawImage(img, this.clipX, this.clipY, this.clipHeight, this.clipWidth)
     }
