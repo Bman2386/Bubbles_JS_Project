9d3d38fb0ffc92baab13677cd064b4c8aa57e5e9
Brendon <brendon.biagi@gmail.com>
2023-05-11 18:19:09 -0400

    fix linter errors in cloud.js

--- src/gameFiles/cloud.js
+++ src/gameFiles/cloud.js
@@ -1,28 +1,28 @@
 
 export default class Cloud {
     constructor(canvasWidth, canvasHeight) {
-    const canvas = document.getElementById('myCanvas');
-        this.ctx = canvas.getContext("2d");
-        this.canvasWidth = canvasWidth;
-        this.canvasHeight = canvasHeight;
+    const canvas = document.getElementById('myCanvas')
+        this.ctx = canvas.getContext("2d")
+        this.canvasWidth = canvasWidth
+        this.canvasHeight = canvasHeight
 
-        this.cloudHeight = 50;
-        this.cloudWidth = 50;
-        this.cloudX = 0;
-        this.cloudY = 0;
+        this.cloudHeight = 50
+        this.cloudWidth = 50
+        this.cloudX = 0
+        this.cloudY = 0
 
         this.drawCloud = this.drawCloud.bind(this)
     }
 
     drawCloud(ctx, deadX, deadY){
-        const img = new Image();
+        const img = new Image()
         img.src = './images/cloud.jpg'
         ctx.drawImage(img, this.cloudX, this.cloudY, this.cloudWidth, this.cloudHeight)
         this.move(deadX, deadY)
     }
 
     move(deadX, deadY){
-        this.cloudX = deadX;
-        this.cloudY = deadY;  
+        this.cloudX = deadX
+        this.cloudY = deadY  
     }
 }
