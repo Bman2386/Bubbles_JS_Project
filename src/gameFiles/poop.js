781c94aaa5de9e29241676de36f7d8d38ab65bcc
Brendon <brendon.biagi@gmail.com>
2023-05-12 11:15:28 -0400

    fix linter error in poop.js

--- src/gameFiles/poop.js
+++ src/gameFiles/poop.js
@@ -2,29 +2,29 @@
 
 export default class Poop {
     constructor(canvasWidth, canvasHeight) {
-        const canvas = document.getElementById('myCanvas');
-            this.ctx = canvas.getContext("2d");
-            this.canvasWidth = canvasWidth;
-            this.canvasHeight = canvasHeight;
+        const canvas = document.getElementById('myCanvas')
+            this.ctx = canvas.getContext("2d")
+            this.canvasWidth = canvasWidth
+            this.canvasHeight = canvasHeight
     
-            this.poopHeight = 50;
-            this.poopWidth = 50;
+            this.poopHeight = 50
+            this.poopWidth = 50
             this.poopX 
             this.poopY 
     
-            this.poopSrc = './images/poop.jpg';
+            this.poopSrc = './images/poop.jpg'
             this.drawPoop = this.drawPoop.bind(this)
         }
         drawPoop(ctx) {
-            const img = new Image();
-            img.src = this.poopSrc;
-            ctx.drawImage(img, this.poopX, this.poopY, this.poopHeight, this.poopWidth);
+            const img = new Image()
+            img.src = this.poopSrc
+            ctx.drawImage(img, this.poopX, this.poopY, this.poopHeight, this.poopWidth)
             this.fall()
         }
 
         start(tempX, tempY) {
-            this.poopX = tempX;
-            this.poopY = tempY;
+            this.poopX = tempX
+            this.poopY = tempY
         }
         fall(){
             this.poopY += 5
