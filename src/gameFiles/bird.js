e565487187a311b8c5f20ca732846d6454177cbf
Brendon <brendon.biagi@gmail.com>
2023-06-19 10:21:40 -0400

    use solid on bird.js

--- src/gameFiles/bird.js
+++ src/gameFiles/bird.js
@@ -6,23 +6,23 @@ export default class Enemy {
         this.ctx = canvas.getContext("2d")
         this.canvasWidth = canvasWidth
         this.canvasHeight = canvasHeight
-        // size of bird
+        
         this.birdHeight = 75
         this.birdWidth = 75
-        // use Math.rndom to generate birds in random positions
-        // with a min floor of 200
+        
         this.birdY = Math.floor(Math.random() * Math.floor(200))
         this.birdX = 0
-        // drawBird is defined below, it's the function that actually draws the bird
-        // we need to bind "this" to it so we don't lose it upon creation
+        
         this.drawBird = this.drawBird.bind(this)
+       
     }
 
+    
     drawBird(ctx) {
         const img = new Image()
+        const moveBirdAccrossScreen = 7
         img.src = birdImg
         ctx.drawImage(img, this.birdX, this.birdY, this.birdWidth, this.birdHeight)
-        // adding 7 to the birdX makes it move accross screen
-        this.birdX += 7
+        this.birdX += moveBirdAccrossScreen
     }
 }
